5e284e582d766841a9d58ec6a202fb9f61f61a98
hyppermoon <robert.mazurkiewicz1@gmail.com>
2026-02-05 20:17:51 +0100

    Add checkout validation test for missing first name

--- src/test/java/pages/CheckoutStepOnePage.java
+++ src/test/java/pages/CheckoutStepOnePage.java
@@ -9,6 +9,7 @@ public class CheckoutStepOnePage extends BasePage {
     private final By lastName  = By.cssSelector("[data-test='lastName']");
     private final By zipCode   = By.cssSelector("[data-test='postalCode']");
     private final By continueButton = By.cssSelector("[data-test='continue']");
+    private final By errorMessage = By.cssSelector("[data-test='error']");
 
     public CheckoutStepOnePage(WebDriver driver) {
         super(driver);
@@ -20,5 +21,25 @@ public class CheckoutStepOnePage extends BasePage {
         type(zipCode, zip);
         click(continueButton);
         return new CheckoutStepTwoPage(driver);
+    }
+
+    public CheckoutStepOnePage fillForm(String fName, String lName, String zip) {
+        type(firstName, fName);
+        type(lastName, lName);
+        type(zipCode, zip);
+        return this;
+    }
+
+    public CheckoutStepOnePage clickContinue() {
+        click(continueButton);
+        return this;
+    }
+
+    public boolean isErrorVisible() {
+        return !driver.findElements(errorMessage).isEmpty() && waitVisible(errorMessage).isDisplayed();
+    }
+
+    public String getErrorMessage() {
+        return waitVisible(errorMessage).getText();
     }
 }
--- src/test/java/tests/CheckoutTests.java
+++ src/test/java/tests/CheckoutTests.java
@@ -26,5 +26,28 @@ class CheckoutTests extends BaseTest {
 
         assertTrue(stepTwo.isLoaded(), "Checkout summary page should be loaded");
     }
+
+    @Test
+    void shouldShowErrorWhenFirstNameIsMissing() {
+        var inventory = new LoginPage(driver)
+                .open(ConfigLoader.getBaseUrl())
+                .loginExpectSuccess(TestUsers.STANDARD_USER, TestUsers.PASSWORD);
+
+        var cart = inventory
+                .addBackpackToCart()
+                .goToCart();
+
+        var stepOne = cart.checkout();
+
+        stepOne.fillForm("", "Kowalski", "00-001")
+                .clickContinue();
+
+        assertTrue(stepOne.isErrorVisible(), "Error message should be visible");
+        assertTrue(
+                stepOne.getErrorMessage().contains("First Name is required"),
+                "Expected first-name-required validation message"
+        );
+    }
+
 }
 
